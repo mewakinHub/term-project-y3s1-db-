1059a4cd8685c06334de45a66588ba282236c1b9
twtrp <134314926+twtrp@users.noreply.github.com>
2023-11-01 21:50:00 +0700

    update

--- ggg-app/src/app/(main)/layout.jsx
+++
@@ -1,7 +0,0 @@
-import '@/styles/main.css'
-
-export default function layoutMain({ children }) {
-  return (
-    <div className='root main'>{children}</div>
-  )
-}
---
+++ ggg-app/src/app/(mainapp)/layout.jsx
@@ -0,0 +1,7 @@
+import '@/styles/mainapp.css'
+
+export default function layoutMain({ children }) {
+  return (
+    <div className='root mainapp'>{children}</div>
+  )
+}
