161b2bf968de2357a6fc225cd134eb29face60b2
twtrp <134314926+twtrp@users.noreply.github.com>
2023-11-03 22:42:51 +0700

    navbar finish

--- ggg-app/src/app/(mainapp)/layout.jsx
+++ ggg-app/src/app/(mainapp)/layout.jsx
@@ -16,44 +16,65 @@ export default function layoutMain({ children }) {
   return (
     <div className='root mainapp'>
         <nav>
-          <div className='nav-main'>
-            <Image className='center logo'
-              src={logo}
-              style={{maxWidth: '70px', height: 'auto'}}
-            />
-            <div className='nav-main-buttons'>
-              <Link href={Route('featured')} passHref className='button-wrapper'>
-                <button className={'featured'.concat(' ',pathname == Route('featured') ? ' active' : '')}>
-                  {Icon('featured')}
-                  Featured
-                </button>
-              </Link>
-              <Link href={Route('browse')} passHref className='button-wrapper'>
-                <button className={'browse'.concat(' ',pathname == Route('browse') ? ' active' : '')}>
-                  {Icon('browse')}
-                  Browse
-                </button>
-              </Link>
-              <Link href={Route('library')} passHref className='button-wrapper'>      
-                <button className={'library'.concat(' ',pathname == Route('library') ? ' active' : '')}>
-                  {Icon('library')}
-                  Library
-                </button>
-              </Link>
-              <Link href={Route('friends')} passHref className='button-wrapper'>      
-                <button className={'friends'.concat(' ',pathname == Route('friends') ? ' active' : '')}>
-                  {Icon('friends')}
-                  Friends
-                </button>
-              </Link>
+          <div className='nav-wrapper'>
+            <div className='nav-main'>
+              <Image className='center logo'
+                src={logo}
+                style={{maxWidth: '70px', height: 'auto'}}
+              />
+              <div className='nav-main-buttons'>
+                <Link href={Route('featured')} passHref className='button-wrapper'>
+                  <button className={'featured'.concat(' ',pathname == Route('featured') ? ' active' : '')}>
+                    {Icon('featured')}
+                    Featured
+                  </button>
+                </Link>
+                <Link href={Route('browse')} passHref className='button-wrapper'>
+                  <button className={'browse'.concat(' ',pathname == Route('browse') ? ' active' : '')}>
+                    {Icon('browse')}
+                    Browse
+                  </button>
+                </Link>
+                <Link href={Route('library')} passHref className='button-wrapper'>      
+                  <button className={'library'.concat(' ',pathname == Route('library') ? ' active' : '')}>
+                    {Icon('library')}
+                    Library
+                  </button>
+                </Link>
+                <Link href={Route('friends')} passHref className='button-wrapper'>      
+                  <button className={'friends'.concat(' ',pathname == Route('friends') ? ' active' : '')}>
+                    {Icon('friends')}
+                    Friends
+                  </button>
+                </Link>
+              </div>
+              <hr/>
             </div>
-          </div>
-          <div className='nav-installed'>
-            
-          </div>
-          <div className='nav-you'>
-
-          </div>
+            <div className='nav-installed'>
+              <div className='installed-header'>
+                <p>Installed</p>
+                <div className='iconbutton-wrapper'>
+                  {Icon('adjust')}
+                </div>
+              </div>
+            </div>
+            <div className='nav-user'>
+              <hr/>
+              <div className='button-wrapper'>
+                <button className='full'>
+                  <div className='user-container'>
+                    <Image className='pfp'
+                      src={pfp}
+                    />
+                    <div className='user-info'>
+                      <p className='username'>username</p>
+                      <p className='funds'>฿0.00</p>
+                    </div>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div> 
         </nav>
         {children}
     </div>
