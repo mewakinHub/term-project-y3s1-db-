0aec7e3159c45e1fd5de5b0cd53504871cb7e9b3
twtrp <134314926+twtrp@users.noreply.github.com>
2023-11-18 23:14:16 +0700

    update

--- ggg-app/signin.php
+++ ggg-app/signin.php
@@ -15,34 +15,32 @@
   <title>GGG - Sign in</title>
   <link rel="stylesheet" href="style/signin.css">
 </head>
-<body>
-  <main class="signin">
-      <div class="signin-container">
-         <img class="logofull" src="asset/logofull.png"
-            style="max-width: 185px; height: auto;" draggable="false"
-         />
-         <div class="message-container">
-            <h2 class="text-center">Welcome!</h2>
-         </div>
-         <form class="form-signin" action="featured.php">
-            <div class="inputicon-container email">
-               <input class="input-iconned"
-                  type="text" name="email" placeholder="E-mail" required
-               />
-               <?php Icon("mail") ?>
-            </div>
-            <div class="inputicon-container password">
-               <input class="input-iconned"
-                  type="password" name="password" placeholder="Password" required
-               />
-               <?php Icon("key") ?>
-            </div>
-            <button type="submit">Sign in</button>
-         </form>
-         <a href="signup.php" draggable="false">
-            <button class="button-signup" type="button">Create account</button>
-         </a>
+<body class="signin">
+   <div class="signin-container">
+      <img class="logofull" src="asset/logofull.png"
+         style="max-width: 185px; height: auto;" draggable="false"
+      />
+      <div class="message-container">
+         <h2 class="text-center">Welcome!</h2>
       </div>
-   </main>
+      <form class="form-signin" action="featured.php">
+         <div class="inputicon-container email">
+            <input class="input-iconned"
+               type="text" name="email" placeholder="E-mail" required
+            />
+            <?php Icon("mail") ?>
+         </div>
+         <div class="inputicon-container password">
+            <input class="input-iconned"
+               type="password" name="password" placeholder="Password" required
+            />
+            <?php Icon("key") ?>
+         </div>
+         <button type="submit">Sign in</button>
+      </form>
+      <a href="signup.php" draggable="false">
+         <button class="button-signup" type="button">Create account</button>
+      </a>
+   </div>
 </body>
 </html>
--- ggg-app/signup.php
+++ ggg-app/signup.php
@@ -15,46 +15,44 @@
   <title>GGG - Sign up</title>
   <link rel="stylesheet" href="style/signin.css">
 </head>
-<body>
-  <main class="signin">
-      <div class="signin-container">
-         <img class="logofull" src="asset/logofull.png"
-            style="max-width: 185px; height: auto;" draggable="false"
-         />
-         <div class="message-container">
-            <a class="button-back" href="signin.php" draggable="false">
-               <?php Icon('back') ?>
-            </a>
-            <h2 class="signin-message text-center">Create account</h2>
-         </div>
-         <form class="form-signin" action="featured.php" method="post">
-            <div class="inputicon-container email">
-               <input class="input-iconned"
-                  type='text' name='email' placeholder='E-mail' required
-               />
-               <?php Icon('mail') ?>
-            </div>
-            <div class="inputicon-container password">
-               <input class="input-iconned"
-                  type='password' name='password' placeholder='Password' required
-               />
-               <?php Icon('key') ?>
-            </div>
-            <div class="inputicon-container password">
-               <input class="input-iconned"
-                  type='password' name='confirmpassword' placeholder='Confirm Password' required
-               />
-               <?php Icon('key') ?>
-            </div>
-            <div class="inputicon-container username">
-               <input class="input-iconned"
-                  type='text' name='username' placeholder='Display Name' 
-               />
-               <?php Icon('id') ?>
-            </div>
-            <button type="submit">Sign up</button>
-         </form>
+<body class="signin">
+   <div class="signin-container">
+      <img class="logofull" src="asset/logofull.png"
+         style="max-width: 185px; height: auto;" draggable="false"
+      />
+      <div class="message-container">
+         <a class="button-back" href="signin.php" draggable="false">
+            <?php Icon('back') ?>
+         </a>
+         <h2 class="signin-message text-center">Create account</h2>
       </div>
-   </main>
+      <form class="form-signin" action="featured.php" method="post">
+         <div class="inputicon-container email">
+            <input class="input-iconned"
+               type='text' name='email' placeholder='E-mail' required
+            />
+            <?php Icon('mail') ?>
+         </div>
+         <div class="inputicon-container password">
+            <input class="input-iconned"
+               type='password' name='password' placeholder='Password' required
+            />
+            <?php Icon('key') ?>
+         </div>
+         <div class="inputicon-container password">
+            <input class="input-iconned"
+               type='password' name='confirmpassword' placeholder='Confirm Password' required
+            />
+            <?php Icon('key') ?>
+         </div>
+         <div class="inputicon-container username">
+            <input class="input-iconned"
+               type='text' name='username' placeholder='Display Name' 
+            />
+            <?php Icon('id') ?>
+         </div>
+         <button type="submit">Sign up</button>
+      </form>
+   </div>
 </body>
 </html>
