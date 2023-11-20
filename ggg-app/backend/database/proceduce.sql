0a59f6d35dc1b6beb815138dd52c1cea38e68bf9
kavinrath <kavinrathit@gmail.com>
2023-11-21 01:37:08 +0700

    buy(add)game finish

--- ggg-app/backend/database/proceduce.sql
+++ ggg-app/backend/database/proceduce.sql
@@ -7,3 +7,22 @@ BEGIN
 END //
 
 DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE AddInstalledGame(IN user_id INT, IN game_id INT)
+BEGIN
+    -- Check if the game is already owned and installed by the user
+    IF NOT EXISTS (SELECT 1 FROM `own` WHERE `userID` = user_id AND `gameID` = game_id) THEN
+        -- Insert the new game as owned and installed
+        INSERT INTO `own` (`userID`, `gameID`, `playtime`, `installed`)
+        VALUES (user_id, game_id, 0, 1);
+    ELSE
+        -- If the game is already owned, just update the installed status
+        UPDATE `own` SET `installed` = 1
+        WHERE `userID` = user_id AND `gameID` = game_id;
+    END IF;
+END$$
+
+DELIMITER ;
---
+++ ggg-app/backend/game/add_game.php
@@ -0,0 +1,45 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once("connect.php");
+
+// Check if the form was submitted and the REQUEST_METHOD is set
+if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
+    // Call the AddInstalledGame stored procedure
+    $stmt = $conn->prepare("CALL AddInstalledGame(?, ?)");
+    $stmt->bind_param("ii", $userID, $gameID);
+    $userID = $_POST['userID']; 
+    $gameID = $_POST['gameID']; 
+    
+    if ($stmt->execute()) {
+        echo "<p>Game added to installed list!</p>";
+    } else {
+        echo "<p>Error: " . $stmt->error . "</p>";
+    }
+    
+    $stmt->close();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add Installed Game</title>
+</head>
+<body>
+    <h2>Add Installed Game</h2>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <label for="userID">Your User ID:</label>
+        <input type="text" id="userID" name="userID" required><br><br>
+        
+        <label for="gameID">Game ID:</label>
+        <input type="text" id="gameID" name="gameID" required><br><br>
+        
+        <input type="submit" value="Add Game as Installed">
+    </form>
+</body>
+</html>
