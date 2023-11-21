dc82a57d45e71b176cdff8283f9dcfc4a7608653
Mewakin <mewteetawat@gmail.com>
2023-11-22 04:53:55 +0700

    .

--- ggg_admin/Lab 9/Assignment9.2/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-   $mysqli = new mysqli('localhost','root','root','staff');
-   if($mysqli->connect_errno){
-      echo $mysqli->connect_errno.": ".$mysqli->connect_error;
-   }
-?>
--- ggg_admin/proj/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conn = mysqli_connect('localhost','root','root','testcase') or die('connection failed');
-
-?>
