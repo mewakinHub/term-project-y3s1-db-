0c474311938bc8ce4cf77731cb76d290029350d1
kavinrath <kavinrathit@gmail.com>
2023-11-22 00:58:58 +0700

    updatesearh

--- ggg-app/backend/game/search.php
+++ ggg-app/backend/game/search.php
@@ -3,47 +3,30 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once("connect.php");
-
-// Check if the 'userID' parameter is set
-if (isset($_GET['userID'])) {
-    $currentUserId = $_GET['userID'];
-
-    // Fetch games that are owned but not installed
-    $sql = "SELECT g.gameID, g.name, g.icon FROM own o
-            JOIN game g ON o.gameID = g.gameID
-            WHERE o.userID = ? AND o.installed = 0";
-
-    $statement = $conn->prepare($sql);
-    
-    if ($statement === false) {
-        die("Prepare failed: " . htmlspecialchars($conn->error));
+// Check if the 'search' parameter is set
+if (isset($_GET['search'])) {
+    $search = mysqli_real_escape_string($conn, $_GET['search']);
+    // echo "Search Query: " . $search . "<br>";
+    $sql = "SELECT gameID, name, price, icon, poster FROM game WHERE name LIKE '%$search%' OR shortdesc LIKE '%$search%'";
+    $result = $conn->query($sql);
+    // Check for query execution errors
+    if (!$result) {
+        die("Query failed: " . $conn->error);
     }
-
-    $statement->bind_param("i", $currentUserId);
-    $statement->execute() or die("Error: " . htmlspecialchars($statement->error));
-    $result = $statement->get_result();
-
-    // Display the owned but not installed games
-    echo "<div class='owned-games'>"; // Start the container for owned but not installed games
+    // Display the search results
+    echo "<div class='searched-games'>"; // Start the container for searched games
     while ($row = $result->fetch_assoc()) {
         echo "<div class='game'>"; // Game container
-        // Display the game icon
-        echo '<img src="data:image/jpeg;base64,' . base64_encode($row['icon']) . '" alt="' . htmlspecialchars($row['name']) . '" class="game-icon">';
-        // Display the game name
-        echo '<p class="game-name">' . htmlspecialchars($row['name']) . '</p>';
-
-        // Form for installing the game
-        echo '<form method="POST" action="install_game.php">';
-        echo '<input type="hidden" name="userID" value="' . htmlspecialchars($currentUserId) . '">';
-        // Make sure to output the gameID
-        echo '<input type="hidden" name="gameID" value="' . htmlspecialchars($row['gameID']) . '">';
-        echo '<input type="submit" name="installGame" value="Install">';
-        echo '</form>'; 
-
-        echo "</div>"; // End game container
+        // Display game information
+        echo "Game ID: " . $row['gameID'] . "<br>";
+        echo "Name: " . $row['name'] . "<br>";
+        echo "Price: ฿" . $row['price'] . "<br>";
+        // Display the game poster
+        echo '<img src="data:image/jpeg;base64,' . base64_encode($row['poster']) . '" alt="Poster for ' . htmlspecialchars($row['name']) . '" class="game-poster">';
+        echo "</div>"; 
     }
-    echo "</div>"; // End the container for owned but not installed games
+    echo "</div>"; 
 } else {
-    echo "User ID not specified.";
+    echo "Search parameter not specified.";
 }
 ?>
