29feff0dabef125b85d5ca9eb83bd1a243c36571
kavinrath <kavinrathit@gmail.com>
2023-11-22 02:22:42 +0700

    search_game_library_perfect

--- ggg-app/backend/library/search.php
+++ ggg-app/backend/library/search.php
@@ -6,50 +6,48 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once("connect.php");
+// Check the connection
 
-// Output the current userID in the session
-// echo "User ID in session: " . $_SESSION['userID'] . "<br>";
-
-// Check if the 'userID' is set in the URL
+// Retrieve the user ID from the form submission
 if (isset($_GET['userID'])) {
-    $userId = $_GET['userID'];
+    $userID = $_GET['userID'];
 
-    // Set the user ID in the session for future use
-    $_SESSION['userID'] = $userId;
-} elseif (isset($_SESSION['userID'])) {
-    $userID = $_SESSION['userID'];
+    // Retrieve the search term from the form submission
+    if (isset($_GET['search'])) {
+        $searchTerm = '%' . $_GET['search'] . '%'; // Use '%' for partial matching
+
+        // Use prepared statements to prevent SQL injection
+        $stmt = $conn->prepare("SELECT own.*, game.poster FROM own INNER JOIN game ON own.gameID = game.gameID WHERE own.userID = ? AND own.gameID IN (SELECT gameID FROM game WHERE game.name LIKE ?)");
+
+
+
+        if (!$stmt) {
+            die("Error in statement preparation: " . $conn->error);
+        }
+
+        $stmt->bind_param("is", $userID, $searchTerm);
+
+        if (!$stmt->execute()) {
+            die("Error in statement execution: " . $stmt->error);
+        }
+
+        $result = $stmt->get_result();
+
+        // Output the results
+        echo "<h2>Search Results:</h2>";
+        echo "<ul>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<li>Game ID: " . $row['gameID'] . ", Playtime: " . $row['playtime'] . ", Installed: " . $row['installed'] . "</li>";
+        }
+        echo "</ul>";
+
+        $stmt->close();
+    } else {
+        echo "Search term not specified.";
+    }
 } else {
-    // If user ID is not in session or URL, exit with an error message
-    die("User ID not specified.");
+    echo "User ID not specified.";
 }
 
-// Fetch games that the user owns (installed and not installed)
-$sql = "SELECT g.gameID, g.name, g.icon FROM game g
-        LEFT JOIN own o ON g.gameID = o.gameID
-        WHERE o.userID = ?";
-
-$statement = $conn->prepare($sql);
-
-if ($statement === false) {
-    die("Prepare failed: " . htmlspecialchars($conn->error));
-}
-
-$statement->bind_param("i", $currentUserId);
-$statement->execute() or die("Error: " . htmlspecialchars($statement->error));
-$result = $statement->get_result();
-
-// Display the owned games
-echo "<div class='owned-games'>"; // Start the container for owned games
-while ($row = $result->fetch_assoc()) {
-    echo "<div class='game'>"; // Game container
-    // Display the game icon
-    echo '<img src="data:image/jpeg;base64,' . base64_encode($row['icon']) . '" alt="' . htmlspecialchars($row['name']) . '" class="game-icon">';
-    // Display the game name
-    echo '<p class="game-name">' . htmlspecialchars($row['name']) . '</p>';
-
-    // Add other game information as needed
-
-    echo "</div>"; // End game container
-}
-echo "</div>"; // End the container for owned games
+$conn->close();
 ?>
--- ggg-app/backend/library/search_game.php
+++ ggg-app/backend/library/search_game.php
@@ -1,35 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Game Search</title>
-</head>
-<body>
+<?php
+// Start the session
+session_start();
 
-    <h1>Game Search in Library</h1>
+// Check if the 'userID' is set in the query parameters
+if (isset($_GET['userID'])) {
+    $_SESSION['userID'] = $_GET['userID'];
+}
 
-    <?php
-    // Start the session
-    session_start();
-    echo "User ID in session: " . $_SESSION['userID'] . "<br>";
+echo "User ID in session: " . $_SESSION['userID'] . "<br>";
 
+// Check if the 'userID' is set in the session, if not, exit with an error message
+if (!isset($_SESSION['userID'])) {
+    die("User ID not specified.");
+}
 
-    // Check if the 'userID' is set in the session, if not, exit with an error message
-    if (!isset($_SESSION['userID'])) {
-        die("User ID not specified.");
-    }
+// Check if 'gameID' is set in the form submission
+if (isset($_GET['gameID'])) {
+    $gameID = $_GET['gameID'];
+    // Handle the gameID as needed
+} else {
+    // 'gameID' is not set, handle the situation accordingly
+    echo "Game ID not specified.";
+}
 
-    $currentUserId = $_SESSION['userID'];
-    ?>
+?>
 
-    <form action="search.php" method="GET">
-        <!-- Include the user ID in the form as a hidden input -->
-        <input type="hidden" name="userID" value="<?php echo $currentUserId; ?>">
-        <label for="search">Search:</label>
-        <input type="text" name="search" id="search" placeholder="Enter game name">
-        <button type="submit">Search</button>
-    </form>
+<form action="search.php" method="GET">
+    <!-- Include the user ID in the form as a hidden input -->
+    <input type="hidden" name="userID" value="<?php echo $_SESSION['userID']; ?>">
+    <label for="search">Search:</label>
+    <input type="text" name="search" id="search" placeholder="Enter game name">
+    
+    <!-- Include the game ID in the form as a hidden input -->
+    <input type="hidden" name="gameID" value="<?php echo $gameID; ?>">
 
-</body>
-</html>
+    <button type="submit">Search</button>
+</form>
