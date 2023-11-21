f6dd1d49d84b61b656d77002d63b3757bbdaa885
kavinrath <kavinrathit@gmail.com>
2023-11-22 00:05:28 +0700

    playtime

---
+++ ggg-app/backend/game/search_game.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Game Search</title>
+</head>
+<body>
+
+    <h1>Game Search</h1>
+
+    <form action="search.php" method="GET">
+        <label for="search">Search:</label>
+        <input type="text" name="search" id="search" placeholder="Enter game name">
+        <button type="submit">Search</button>
+    </form>
+
+</body>
+</html>
